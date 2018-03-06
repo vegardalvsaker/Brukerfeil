89b85a2694fdbd40c3a0ee310dd49e93c8c259b6
vegardalvsaker <vegarda96@gmail.com>
2018-03-06 21:44:02 +0100

    Fjernet en utdatert funksjon

--- oblig2/src/oppgave1/fileinfo.go
+++ oblig2/src/oppgave1/fileinfo.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main(){
-	filnavn := os.Args[1]
+	filnavn := `C:\Users\Vegard\Documents\dev\Go\is-105\Brukerfeil\oblig2\src\oppgave1\filnavn.txt`
 
 	//os.Lstat returnerer en FileInfo av filen
 	fi, err := os.Lstat(filnavn)
@@ -16,7 +16,7 @@ func main(){
 		}
 
 	//fi.Mode() returner en FileMode av FileInfo.
-	mode := fi.Mode();
+	mode := fi.Mode()
 
 	//Filnavn
 	fmt.Println("Information about file <", fi.Name(), ">")
@@ -45,6 +45,8 @@ func main(){
 	}
 	//Unix permissions
 	//unixrep := mode&os.ModeSocket
+	var tekst = int(mode)
+	fmt.Println(tekst)
 	fmt.Println("Has Unix permission bits:", mode)
 
 	//Append
@@ -78,72 +80,3 @@ func main(){
 		fmt.Println("Is a symbolic link")
 	}
 }
-
-/*func main() {
-	    filnavn := `C:\Users\Vegard\Documents\dev\Go\is-105\Brukerfeil\oblig2\src\oppgave1\filnavn.txt`
-		fi, err := os.Lstat(filnavn)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//Filnavn
-		mode := fi.Mode();
-		fmt.Println("Information about file <", fi.Name(), ">")
-
-        //Size
-        intBytes := fi.Size()
-        bytes := float64(intBytes) //Gjør int om til float64
-
-		KB := bytes / 1000
-		MB := KB / 1000
-		GB := MB / 1000
-		fmt.Println("Size:", fi.Name(), "in bytes, KB, MB and GB:")
-        fmt.Println(bytes, "bytes,", KB, "KB,", MB, "MB,", GB, "GB")
-
-        //Directory
-        if mode.IsDir() != true {
-        	fmt.Println("Is not a directory")
-		} else {
-			fmt.Println("Is a directory")
-		}
-		//Regular
-		if mode.IsRegular() != true {
-			fmt.Println("Is not a regular file")
-		} else {
-			fmt.Println("Is a regular file")
-		}
-		//Unix permissions
-		//unixrep := mode&os.ModeSocket
-		fmt.Println("Has Unix permission bits:", mode)
-
-	    //Append
-	    if mode&os.ModeAppend == 0 {
-	    	fmt.Println("Is not append only")
-		} else {
-			fmt.Println("Is append only")
-		}
-
-		//Device file
-		if mode&os.ModeDevice == 0 {
-			fmt.Println("Is not a device file")
-		} else {
-			fmt.Println("Is a device file")
-		}
-
-		//Unix character
-		if mode&os.ModeCharDevice == 0 {
-			fmt.Println("Is not a Unix character device")
-		} else {
-			fmt.Println("Is a Unix character device")
-		}
-
-		//Unix block
-		//if mode&os.
-
-		//Symbolic link
-		if mode&os.ModeSymlink == 0 {
-			fmt.Println("Is not a symbolic link")
-		} else {
-			fmt.Println("Is a symbolic link")
-		}
-}
-*/
