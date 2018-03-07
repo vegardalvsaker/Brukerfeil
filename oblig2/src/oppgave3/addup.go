73460470fbc63b1edcb178440d2ee57df680d1f3
vegardalvsaker <vegarda96@gmail.com>
2018-03-07 20:49:05 +0100

    La til funksjonalitet for å skrive inn tall fra terminal

--- oblig2/src/oppgave3/addup.go
+++ oblig2/src/oppgave3/addup.go
@@ -3,19 +3,59 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"os/signal"
+	"syscall"
+	"bufio"
+	"strconv"
+	"strings"
+	"log"
 )
 
 var ch = make(chan int, 2)
 
-var tall1 int = 4
-var tall2 int = 8
-
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number: ")
+	tall1Str, _ := reader.ReadString('\n')
+	fmt.Print("Enter one more number: ")
+	tall2Str, _ := reader.ReadString('\n')
+
+	c := strings.Replace(tall1Str, "\n", "", -1)
+	d := strings.Replace(tall2Str, "\n", "", -1)
+
+	tall1, err := strconv.Atoi(c)
+	errHandling(err)
+
+	tall2, err2 := strconv.Atoi(d)
+	errHandling(err2)
+
+	oppgave3a(tall1, tall2)
+}
+
+func errHandling(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func oppgave3a(tall1, tall2 int) {
 	//Funksjon A
+	chsig := make(chan os.Signal, 1)
+	signal.Notify(chsig, syscall.SIGINT)
+	go func() {
+		for sig := range chsig {
+			switch sig {
+			case syscall.SIGINT:
+				fmt.Println("\n Sigint received. Shutting down safely")
+				os.Exit(0)
+			}
+		}
+	}()
 	go func() {
 		ch <- tall1
 		ch <- tall2
-		time.Sleep(1000)
+		time.Sleep(1)
 		fmt.Println(<- ch)
 		fmt.Println("Ferdig")
 	}()
