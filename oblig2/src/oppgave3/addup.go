db99314cdbdd7c4c9f8712163a45d481a6f146d6
vegardalvsaker <vegarda96@gmail.com>
2018-03-08 17:33:21 +0100

    Små endringer + kommentering

--- oblig2/src/oppgave3/addup.go
+++ oblig2/src/oppgave3/addup.go
@@ -15,33 +15,35 @@ import (
 var ch = make(chan int, 2)
 
 func main() {
+	//Aktiverer sigInt-håndtering
+	sigintHandtering()
+
+	//Leser inn to tall fra terminal
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter a number: ")
 	tall1Str, _ := reader.ReadString('\n')
 	fmt.Print("Enter one more number: ")
 	tall2Str, _ := reader.ReadString('\n')
 
-	c := strings.Replace(tall1Str, "\n", "", -1)
-	d := strings.Replace(tall2Str, "\n", "", -1)
+	//Forsikrer at programmet fungerer også på Windows
+	c := strings.Replace(tall1Str, "\r", "", -1)
+	d := strings.Replace(tall2Str, "\r", "", -1)
 
-	tall1, err := strconv.Atoi(c)
+	e := strings.Replace(c, "\n", "", -1)
+	f := strings.Replace(d, "\n", "", -1)
+
+	//Gjør inputen fra stdin om til int slik at vi kan addere
+	tall1, err := strconv.Atoi(e)
 	errHandling(err)
 
-	tall2, err2 := strconv.Atoi(d)
+	tall2, err2 := strconv.Atoi(f)
 	errHandling(err2)
 
 	oppgave3a(tall1, tall2)
 }
 
-func errHandling(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func oppgave3a(tall1, tall2 int) {
 	//Funksjon A
-	sigintHandtering()
 	go func() {
 		ch <- tall1
 		ch <- tall2
@@ -61,6 +63,15 @@ func oppgave3a(tall1, tall2 int) {
 	time.Sleep(10000000)
 }
 
+//Håndterer errors
+func errHandling(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+/*
+Håndterer en eventuell sigint
+ */
 func sigintHandtering() {
 	chsig := make(chan os.Signal, 1)
 	signal.Notify(chsig, syscall.SIGINT)
--- oblig2/src/oppgave3/sumfromfile.go
+++ oblig2/src/oppgave3/sumfromfile.go
@@ -6,29 +6,39 @@ import (
 	"strings"
 	"strconv"
 	"os"
+	//"time"
 )
-
+/*
+Leser tallene fra filen, og legger dem sammen
+og skriver resultatet inn en fil igjen.
+ */
 func SumFromFile() {
 	data, err := ioutil.ReadFile("enFil.txt")
 	ErrorHandling(err)
 
+	// implementer time.Sleep() for å sjekke at sigint faktisk fungerer
+	//time.Sleep(10000000000)
+
 	string1 := string(data)
-	array := strings.SplitAfter(string1, " ")
+	stringSlice := strings.SplitAfter(string1, " ")
 
-	a := array[0]
-	b := array[1]
+	a := stringSlice[0]
+	b := stringSlice[1]
 
+	//Fjerner mellomrom fra stringen slik at konvertering til int
+	//går smertefritt
 	c := strings.Replace(a, " ", "", -1)
 	d := strings.Replace(b, " ", "", -1)
 
 
-	//Converter c og d til
+	//Converter c og d til int
 	tall1, err := strconv.Atoi(c)
 	ErrorHandling(err)
 	tall2, err := strconv.Atoi(d)
 	ErrorHandling(err)
 	sum := tall1 + tall2
 
+	//Konverterer sum til string slik skrivingen til filen lar seg gjøre
 	sumstring := strconv.Itoa(sum)
 
 	file, err := os.Create("enFil.txt")
