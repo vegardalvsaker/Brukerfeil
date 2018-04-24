78c0f4e6caed9a062e840df6f699a15f799eb9eb
vegardalvsaker <vegarda96@gmail.com>
2018-04-24 21:07:39 +0200

    ferdigstilt oppgave 1

--- oblig3/src/oppgave1/oppgave1.go
+++ oblig3/src/oppgave1/oppgave1.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"net/http"
-	"github.com/gorilla/mux"
 	"time"
 	"fmt"
 )
 func main() {
-
-	router := mux.NewRouter()
-	//Håndterer hva som skjer når en klient aksesserer "/ "
-	router.HandleFunc("/", HelloClient)
+	//Håndterer hva som skjer når en klient aksesserer "/"
+	http.HandleFunc("/", HelloClient)
 
 	srvr := http.Server {
 		Addr:         ":8080",
-		Handler:      router,
 		ReadTimeout:  10* time.Second,
 		WriteTimeout: 10* time.Second,
 	}
