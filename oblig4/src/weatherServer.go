aadc913ede512b82e965d06c8d1eb186f42612fa
vegardalvsaker <vegarda96@gmail.com>
2018-05-09 11:35:38 +0200

    Påbegynt testing. Gjenstår en del

--- oblig4/src/weatherServer.go
+++ oblig4/src/weatherServer.go
@@ -50,7 +50,7 @@ const langAndUnits = "?lang=nb&units=si"
 
 //Server-funksjon som håndterer paths
 func server() {
-	http.HandleFunc("/", index) // setting router rule
+	http.HandleFunc("/", index)
 	http.HandleFunc("/forecast", runForecast)
 	panic(http.ListenAndServe(":8080", nil))
 }
@@ -62,6 +62,10 @@ func main() {
 }
 
 func index (w http.ResponseWriter, r *http.Request) {
+	//Resetter by-variabelen for å fjerne linken på framsiden hvis du velger å gå tilbake til index fra /forecast.
+	by = Sted {
+		Sted: "Søk etter sted...",
+	}
 	//Lager template av HTML-filene
 	tmpl, err := template.ParseFiles("index.html")
 	httpErrorHandling(w, err)
@@ -81,12 +85,16 @@ func index (w http.ResponseWriter, r *http.Request) {
 
 func runForecast (w http.ResponseWriter, r *http.Request) {
 	//Hvis en bruker går på /forecast uten å submitte koordinater
+	latLng = "===9((&%#'``#¤%agsdg"
 	if len(latLng) <= 0 {
+		w.WriteHeader(http.StatusBadRequest)
 		t, err := template.ParseFiles("feil.html")
 		httpErrorHandling(w, err)
 		t.Execute(w, nil)
-	} else {
 
+		//http.Error(w,"Du må søke etter et sted før du kan hente værmeldingen! \nGå tilbake for å gjøre et søk", http.StatusBadRequest)
+
+	} else {
 		//Setter sammen API-urlen sammen med koordinatene
 		url := joinUrlAndCoord()
 
@@ -101,10 +109,8 @@ func runForecast (w http.ResponseWriter, r *http.Request) {
 		executeTilbakemelding(w)
 
 	}
-	//Resetter by-variabelen for å fjerne linken på framsiden hvis du velger å gå tilbake.
-	by = Sted {
-		Sted: "Søk etter sted...",
-	}
+
+
 }
 
 func joinUrlAndCoord() string {
@@ -116,12 +122,12 @@ func joinUrlAndCoord() string {
 //Funksjon for å hente koordinatene og navn til det stedet brukeren ønsker værdata fra
 func getForm(r *http.Request){
 	r.ParseForm()
-	//Sjekker at brukeren gjør et POST-kall til serveren
+	//Sjekker at brukeren gjør et POST-request til serveren
 	if r.Method == "POST" {
 		//Henter data fra form-tags fra HTMLen
 		latLng = r.Form["kords"][0]
 		stedsNavn:= r.Form["by"][0]
-		//Legger
+		//Legger stedet som blir søkt på i to forskjellige structs
 		by = Sted {
 			Sted: stedsNavn,
 		}
@@ -147,12 +153,17 @@ func errorHandling(err error) {
 	}
 }
 
-//Formaterer den uformaterte urlen som inneholder koordinater fra brukeren
-func latLngFormat(url string) string {
-	first := strings.Replace(url, "(", "", -1)
-	second := strings.Replace(first, ")", "", -1)
-	lats := strings.Replace(second, " ", "", -1)
-	return lats
+//Fjerner paranteser fra koordinatene slik at de skal passe inn i darksky-urlen
+func latLngFormat(s string) string {
+	if strings.ContainsAny(s, "()") {
+		first := strings.Replace(s, "(", "", -1)
+		second := strings.Replace(first, ")", "", -1)
+		lats := strings.Replace(second, " ", "", -1)
+		return lats
+	} else {
+		fmt.Errorf("Koordinatene mangler paranteser som pre- og suffix; den så slik ut %v", s)
+		return s
+	}
 }
 
 //Funksjon for å hente og unmarshalle JSON
@@ -172,16 +183,20 @@ func executeTilbakemelding(w http.ResponseWriter) {
 	//Hvis det ikke regner, gjør temperaturvurderinger
 	if weath.Currently.WindSpeed < 8 {
 		if weath.Currently.PrecipIntensity < 1 {
-			if weath.Currently.Temperature >= 20 {
+			if weath.Currently.Temperature >= 16 {
 				t, err := template.ParseFiles("forecast.html", "msg/above20.html")
 				httpErrorHandling(w, err)
 				t.Execute(w, weath)
-			} else if weath.Currently.Temperature < 20 && weath.Currently.Temperature > 10 {
+			} else if weath.Currently.Temperature < 16 && weath.Currently.Temperature > 10 {
 				t, err := template.ParseFiles("forecast.html", "msg/below20.html")
 				httpErrorHandling(w, err)
 				t.Execute(w, weath)
 			} else if weath.Currently.Temperature <= 0 {
 				t, err := template.ParseFiles("forecast.html", "msg/below0.html")
+				httpErrorHandling(w, err)
+				t.Execute(w, weath)
+			} else {
+				t, err := template.ParseFiles("forecast.html", "msg/above0.html")
 				httpErrorHandling(w, err)
 				t.Execute(w, weath)
 			}
--- oblig4/src/weatherServer_test.go
+++ oblig4/src/weatherServer_test.go
@@ -4,34 +4,150 @@ import (
 	"testing"
 	"net/http"
 	"net/http/httptest"
-	"io/ioutil"
-	//"fmt"
 	"fmt"
-	"bytes"
+	"io/ioutil"
+	"encoding/json"
+	"strings"
+	"strconv"
 )
 
-func TestIndex (t *testing.T) {
+const darksky  = "https://api.darksky.net/forecast/a529911b60d81bab2c791732ad9ddf50/58.8532585,5.732945500000028?lang=nb&units=si"
+
+
+func TestIndex ( t *testing.T) {
 	req, err := http.NewRequest("GET", "localhost:8080/", nil)
 	if err != nil {
 		t.Error("could not create request %v", err)
 	}
-	rec := httptest.NewRecorder()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(index)
 
-	index(rec, req)
+	handler.ServeHTTP(rr, req)
 
-	res := rec.Result()
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", err, http.StatusOK)
+	}
+
+}
+
+func TestGetAndUnmarshal (t *testing.T){
+	var test WeatherData
+	//Henter jsondataen i s
+	res, err := http.Get(darksky)
 	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		t.Error("Expected Status OK: 200; got %v", err)
+	errorHandling(err)
+	//Leser kroppen til jsondataen
+	jsonBytes, err2 := ioutil.ReadAll(res.Body)
+	errorHandling(err2)
+	//Legger jsondaten inn i variabelen v
+	errorHandling(json.Unmarshal(jsonBytes, &test))
+	if test.Currently.Summary == "" {
+		t.Error("Does not work")
 	}
-	html, err := ioutil.ReadAll(res.Body)
-	index, err := ioutil.ReadFile("index.html")
+	fmt.Println(test)
+}
 
-	if string(bytes.Trim(html, " ")) != string(bytes.Trim(index, " ")) {
-		t.Error("The request gets the wrong index.html file")
-
+//Tester at /forecast returnerer StatusKode OK: 200 hvis et api-kall til værdatabasen har blitt gjort i forkant
+func TestRunForecast (t *testing.T) {
+	kords := map[string]string{
+		"58.8532585,5.732945500000028":         "Sandnes",
+		"59.32932349999999,18.068580800000063": "Stockholm",
+		"34.0522342,-118.2436849":              "Los Angeles",
+		"-33.9248685,18.424055299999964":       "Cape Town",
+		"37.566535,126.97796919999996":         "Seoul",
+		"55.755826, 37.617299900000035":        "Moskva",
 	}
-	fmt.Printf("%q", html)
-	fmt.Printf("%q", index)
+	corrupt := []string{
+		"askjhda k21237123()",
+		"90120123983180120381083102",
+		"(-1111,3333)",
+		"Oslo",
+		"===9((&%#'``#¤%agsdg",
+		"",
+		"569939,-12",  //10 characters
+		"569939,-123", //11 characters
+	}
 
+
+	for kord, sted := range kords {
+
+		t.Run(sted, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			//Endrer denne variabelen fordi runForecast henter koordinatene som ligger i denne globale variabelen
+			latLng = kord
+
+			req, err := http.NewRequest("GET", "localhost:8080/forecast", nil)
+			if err != nil {
+				t.Error("could not create request %v", err)
+			}
+
+			runForecast(rr, req)
+			res := rr.Result()
+			fmt.Println(res.StatusCode)
+			if res.StatusCode != http.StatusOK {
+				t.Errorf("Expected %v; got %v", http.StatusOK, res.StatusCode)
+			}
+		})
+	}
+	for i, kords := range corrupt {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			//Endrer denne variabelen fordi runForecast henter koordinatene som ligger i denne globale variabelen
+			latLng = kords
+
+			req, err := http.NewRequest("GET", "localhost:8080/forecast", nil)
+			if err != nil {
+				t.Error("could not create request %v", err)
+			}
+
+			runForecast(rr, req)
+			res := rr.Result()
+			fmt.Println(res.StatusCode)
+			if res.StatusCode != http.StatusBadRequest {
+				t.Errorf("Expected %v; got %v", http.StatusOK, res.StatusCode)
+				}
+			})
+		}
+
+}
+
+//Negativ test for runForecast-funksjonen
+func TestRunForecastNoData (t *testing.T) {
+	rr := httptest.NewRecorder()
+	latLng = ""
+
+	req, err := http.NewRequest("GET", "localhost:8080/forecast", nil)
+	if err != nil {
+		t.Error("could not create request %v", err)
+	}
+
+	runForecast(rr, req)
+	res := rr.Result()
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("Status code should be %v; not %v", http.StatusBadRequest, res.StatusCode)
+	}
+}
+
+func TestLatLngFormat (t *testing.T) {
+	t.Run("positiv", func(t *testing.T) {
+		fmt := latLngFormat("(58.8532585, 5.732945500000028)")
+		if strings.ContainsAny(fmt, "( )") {
+			t.Errorf("latLngFormat greide ikke å formatere koordinatene. Resultatet ble %v", fmt)
+		}
+	})
+	t.Run("negativ", func(t *testing.T){
+		expected := "asdljhafkjfh91281028"
+		fmt := latLngFormat(expected)
+		if fmt != "asdljhafkjfh91281028" {
+			t.Errorf("latLngFormat skulle returnere %v; fikk %v", expected, fmt)
+		}
+	})
+	t.Run("negativ2", func(t *testing.T){
+		expected := "(asdljhafkjf h91281028)"
+		fmt := latLngFormat(expected)
+		if fmt != "asdljhafkjfh91281028" {
+			t.Errorf("latLngFormat skulle returnere %v; fikk %v", expected, fmt)
+		}
+	})
 }
